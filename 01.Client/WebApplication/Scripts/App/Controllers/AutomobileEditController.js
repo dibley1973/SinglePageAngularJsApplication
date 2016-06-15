716458f8b65c2578a4351055abb34a6747bb9a4c
dibley1973 <dibley1973@yahoo.co.uk>
2016-06-15 06:52:00 +0100

    20160615_0651_DWDT Added validation

--- 01.Client/WebApplication/Scripts/App/Controllers/AutomobileEditController.js
+++ 01.Client/WebApplication/Scripts/App/Controllers/AutomobileEditController.js
@@ -2,9 +2,12 @@
 
 templateApp.controller('AutomobileEditController',
     function AutomobileEditController($scope) {
-        $scope.completeCar = function (car) {
-            $scope.cars.push(car);
-            $.mobile.changePage('#car-list');
+        $scope.completeCar = function (car, newCarForm) {
+            if (newCarForm.$valid) {
+                $scope.cars.push(car);
+                window.alert('car ' + car.make + ' - ' + car.model + ' saved!');
+                $.mobile.changePage('#car-list');
+            }
         };
         $scope.cancelCar = function () {
             $.mobile.changePage('#car-list');
