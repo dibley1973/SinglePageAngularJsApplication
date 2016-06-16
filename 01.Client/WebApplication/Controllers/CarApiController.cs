af70cefdfb30391fbc4b1b654e6fa4a217e2dfed
dibley1973 <dibley1973@yahoo.co.uk>
2016-06-16 07:09:23 +0100

    20160616_0708_DWDT Started to add webApi

---
+++ 01.Client/WebApplication/Controllers/CarApiController.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Web.Http;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class CarApiController : ApiController
+    {
+        public JToken Get()
+        {
+            var path = System.Web.Hosting.HostingEnvironment.MapPath("/");
+
+            throw new NotImplementedException();
+        }
+    }
+}
